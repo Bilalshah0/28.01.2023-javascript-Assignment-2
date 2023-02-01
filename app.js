6fddab7b716d43d5f33cf4a7a958f4e8a5d78157
Bilalshah0 <bilalshah.sheikh@gmail.com>
2023-02-01 10:29:31 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// ********Question no.01********
+
+// var username ;
+
+// ********Question no.2*********
+
+
+// var myName = ("Bilal Shah");
+
+// ********Question no.3*********
+
+// var message = ("Hello World");
+// alert(message);
+
+// ********Question no.4*********
+
+// var name =("Ahmed ");
+// alert(name)
+
+// var fNmae =("Ali ");
+// alert(fNmae)
+
+// var education =("Intermediate");
+
+// alert(education )
+
+// ********Question no.5*********
+
+// var pizza =("PIZZA  \nPIZZ \nPIZ \nPI \nP")
+// alert(pizza)
+
+
+// ********Question no.6*********
+
+// var email =("bilal@gmail.com");
+// alert( "My email address is " + email);
+
+// ********Question no.7*********
+
+// var book =("A Smarter way to learn Javascript");
+// alert("I am trying to learn from a Book " + book);
+
+// ********Question no.8*********
+
+// document.write("Yah! I can write HTML content through Javascript");
+
+// ********Question no.9*********
+
+// var symboll = ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+// alert(symboll);
+
+
+
+
---
+++ my.js
@@ -0,0 +1,28 @@
+// ********Question no.1*********
+
+// var age =("18");
+// alert("I am " + age + " years old");
+
+// ********Question no.2*********
+
+// var visit = ("10");
+// alert("You have visited this site " + visit + " times");
+
+// ********Question no.3*********
+
+// var birthDate = ("2000");
+// document.write("My birth year is " + birthDate);
+
+
+// ********Question no.4*********
+
+// var visitorName  = ("Ahmed"); 
+// var productTilte = ("T-shirts");
+// var quatity      = ("8");
+
+// document.write( visitorName + " ordered " + quatity + productTilte + " on XYZ Clothing store")
+
+// ********Question no.5*********
+
+
+
